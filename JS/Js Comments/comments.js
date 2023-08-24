8b218b502b9b090bb875bb752fb5290abc14623a
Shahbaz Mirza <weversity.pak@gmail.com>
2023-08-24 11:30:01 +0500

    adding  git

---
+++ JS/Js Comments/comments.js
@@ -0,0 +1,9 @@
+// var num1 = parseInt(prompt("Enter Your Number To Find Square"));
+// // var squarRoot = num1**2;
+// // var cubeRoot = num1**3;
+// num1 -=45;
+
+
+alert(7==7 || 7>9)
+
+// alert(num1);
---
+++ index.js
@@ -0,0 +1,35 @@
+let cars = {name:"Civic", company:"Honda", color:"Red"};
+
+
+alert(cars.color);
+alert(cars[""])
+
+// alert(`the car name is ${cars.name} & the company name is ${cars.company}`);
+
+// var apple = "apple";
+// var banana="banana";
+// var waterMelon="waterMelon";
+// var grapes="grapes";
+                 // 0       1           2           3
+// const fruites = ["apple", "banana", "waterMelon", "grapes"];
+// var animal = "Dog";
+
+// alert(typeof(animal));
+
+// function username(){
+// var name = prompt("Enter Your Name");
+// alert(name);
+// }
+// username();
+
+// let newUser=()=>{
+//     var name = prompt("Enter Your Name");
+//     alert(name);
+// };
+// newUser();
+
+// const firstUser =()=>{
+//     var name = prompt("Enter Your Name");
+//     alert(name);
+// };
+// firstUser();
